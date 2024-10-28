9933199828f7e1667097985cd8ddfc637258506e
Masiche1 <93169988+Masiche1@users.noreply.github.com>
2024-10-28 20:15:09 +0300

    Create Dashboard.js

---
+++ src/components/Dashboard.js
@@ -0,0 +1,13 @@
+// src/components/Dashboard.js
+import React from 'react';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <h1>Presty Hospital Management Dashboard</h1>
+            <p>Welcome to the Presty Hospital Management System!</p>
+        </div>
+    );
+};
+
+export default Dashboard;
