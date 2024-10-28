cb49f07d425555f431a19ff4dd07112a7e0cdcb6
Masiche1 <93169988+Masiche1@users.noreply.github.com>
2024-10-28 20:18:33 +0300

    Create PatientManagement.js

---
+++ src/components/PatientManagement.js
@@ -0,0 +1,13 @@
+// src/components/PatientManagement.js
+import React from 'react';
+
+const PatientManagement = () => {
+    return (
+        <div>
+            <h2>Patient Management</h2>
+            {/* Add patient management functionalities here */}
+        </div>
+    );
+};
+
+export default PatientManagement;
