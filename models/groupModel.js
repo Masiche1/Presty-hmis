cdbda65f2c2d38945b068a35c3b79314c42d5ca8
Masiche1 <93169988+Masiche1@users.noreply.github.com>
2024-10-30 14:41:22 +0300

    Create groupModel.js

---
+++ models/groupModel.js
@@ -0,0 +1,9 @@
+// models/groupModel.js
+const mongoose = require('mongoose');
+
+const groupSchema = new mongoose.Schema({
+    name: { type: String, required: true, unique: true },
+    members: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+});
+
+module.exports = mongoose.model('Group', groupSchema);
