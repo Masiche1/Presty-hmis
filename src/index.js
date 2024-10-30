6ca848ad268fed87ba24f6b0399a585db17f1aaa
Masiche1 <93169988+Masiche1@users.noreply.github.com>
2024-10-30 14:28:21 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ const mpesaRoutes = require('./routes/mpesaRoutes');
 
 // Setting up the Express app
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(cors()); // Enable CORS
