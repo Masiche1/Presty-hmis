d55c0f45b536c882c9659f485dcf6f71a2f89bc5
Masiche1 <93169988+Masiche1@users.noreply.github.com>
2024-10-30 15:17:18 +0300

    Update userRoute.js

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,9 +1,10 @@
 // routes/userRoute.js
 const express = require('express');
-const { createUser, getUsers } = require('../controllers/userController');
+const { createUser, getUsers, loginUser } = require('../controllers/userController');
 const router = express.Router();
 
 router.post('/users', createUser);
 router.get('/users', getUsers);
+router.post('/login', loginUser);
 
 module.exports = router;
